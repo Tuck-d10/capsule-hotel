8feb1712df9cfdf481d252aaf97b53821b4029bf
Tuck-d10 <dtuck@dev-10.com>
2021-07-08 21:07:20 -0500

    added exit option functionality

--- CapsuleHotel.java
+++ CapsuleHotel.java
@@ -5,17 +5,14 @@ public class CapsuleHotel {
     private static final int LINE_LENGTH = 35;
 
     public static void main(String[] args){
-        boolean isHotelActive = true;
         Scanner sc = new Scanner(System.in);
+        boolean isHotelActive = true;
 
-        String[] mainMenuOptions = {"Check In","Check Out","View Guests","Exit"};
-
+        System.out.println("Hello Welcome to the Capsule Hotel!");
         while(isHotelActive){
-            System.out.println("Hello Welcome to the Capsule Hotel!");
-            printMenuHeader(LINE_LENGTH,"Main");
-            printItems(LINE_LENGTH, mainMenuOptions);
-            isHotelActive = false;
+            isHotelActive = mainMenu(sc);
         }
+        System.out.println("Thank you for visiting!");
     }
 
     private static void printDivider(int lineLength, char divider){
@@ -33,25 +30,71 @@ public class CapsuleHotel {
 
     private static void printItems(int lineLength, String[] items){
         for(int i = 0; i < items.length; i++){
-            System.out.printf("|%-"+(lineLength - 2)+"s|\n", items[i]);
+            System.out.printf("|%d) %-"+(lineLength - 5)+"s|\n", (i + 1), items[i]);
             printDivider(lineLength,'-');
         }
     }
 
-    private static void mainMenu(){
+    private static boolean mainMenu(Scanner sc){
+        String[] mainMenuOptions = {"Check In","Check Out","View Guests","Exit"};
+        
+        printMenuHeader(LINE_LENGTH,"Main Menu");
+        printItems(LINE_LENGTH, mainMenuOptions);
 
+        int option = sc.nextInt(); //Need validation to protect against bad input
+
+        switch(option){
+            case 1:
+                checkIn(sc);
+                break;
+            case 2:
+                checkOut(sc);
+                break;
+            case 3:
+                viewGuests(sc);
+                break;
+            case 4:
+                boolean isHotelActive = exit(sc);
+                return isHotelActive;
+            default:
+                System.out.println("Invalid input: use numbers 1-4.");
+        }
+
+        return true;
     }
 
-    private static void checkIn(){
-
+    private static void checkIn(Scanner sc){
+        System.out.println("check in");
     }
 
-    private static void viewGuests(){
-
+    
+    private static void checkOut(Scanner sc){
+        System.out.println("check out");
     }
 
-    private static void exit(){
-        System.out.println("Thank you for visiting the Capsule Hotel");
+    private static void viewGuests(Scanner sc){
+        System.out.println("view guests");
+    }
+
+    private static boolean exit(Scanner sc){
+        System.out.println("Are you sure you want to exit? [y/n]");
+        String answer = "";
+        while(!isYesNo(answer)){        //continues loop until yes or no answer is given
+            userPrompt();
+            answer = sc.nextLine().trim();
+        }
+        return answer.equalsIgnoreCase("n") || answer.equalsIgnoreCase("no");
+    }
+
+    private static void userPrompt(){
+        System.out.print("?: ");
+    }
+
+    private static boolean isYesNo(String answer){
+        return answer != null && (
+            answer.equalsIgnoreCase("y") || answer.equalsIgnoreCase("n") ||
+            answer.equalsIgnoreCase("yes") || answer.equalsIgnoreCase("no")
+        );
     }
 }
 
