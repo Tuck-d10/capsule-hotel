531e32bdc4c32f0c30209932c6f6dcfe274bba5b
Tuck-d10 <dtuck@dev-10.com>
2021-07-08 20:02:25 -0500

    Completed initial menu formatting framework

--- CapsuleHotel.java
+++ CapsuleHotel.java
@@ -1,18 +1,40 @@
 import java.util.Scanner;
 
 public class CapsuleHotel {
+
+    private static final int LINE_LENGTH = 35;
+
     public static void main(String[] args){
         boolean isHotelActive = true;
         Scanner sc = new Scanner(System.in);
 
+        String[] mainMenuOptions = {"Check In","Check Out","View Guests","Exit"};
 
         while(isHotelActive){
-            System.out.println("Type something");
-
-            String foo = sc.nextLine();
-
-            System.out.printf("\"%s\" is what you typed", foo);
+            System.out.println("Hello Welcome to the Capsule Hotel!");
+            printMenuHeader(LINE_LENGTH,"Main");
+            printItems(LINE_LENGTH, mainMenuOptions);
             isHotelActive = false;
+        }
+    }
+
+    private static void printDivider(int lineLength, char divider){
+        for(int i = 0; i < lineLength; i++){
+            System.out.print(divider);
+        }
+        System.out.println();
+    }
+
+    private static void printMenuHeader(int lineLength, String headerText){
+        printDivider(lineLength,'=');
+        System.out.printf("|%-"+(lineLength - 2)+"s|\n", headerText);
+        printDivider(lineLength,'=');
+    }
+
+    private static void printItems(int lineLength, String[] items){
+        for(int i = 0; i < items.length; i++){
+            System.out.printf("|%-"+(lineLength - 2)+"s|\n", items[i]);
+            printDivider(lineLength,'-');
         }
     }
 
